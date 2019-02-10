3cfd117663612105f110d8835d7bda596c6cd4b5
williansjaime <williansjaime@Outlook.com>
2019-02-10 14:50:07 -0200

    Minerando regras de associação
    Nessa aula foi verificada as de alguém comprar cerveja e carvão

---
+++ Itens Frequentes com Eclat.R
@@ -0,0 +1,13 @@
+#Willians 10/2/2019
+#Itens frequentes com Eclat
+#
+#usar o pacote arules
+library(arules)
+transacoe = read.transactions(file.choose(), format="basket", sep=",")
+image(transacoe)
+#gerar mineração de  regras 
+regras = eclat(transacoe, parameter = list(supp = 0.1, maxlen=15))
+inspect(regras)
+#visualisar regras em plot
+plot(regras, method="graph", control = list(type = "items"))
+
